e053bbcbe914e5914f2e34459734c8f11827b7dc
Neelkumar Bhanushali <neel@revosolve.com>
2026-06-13 10:40:05 +0100

    show process ocr button only in pending state

--- veerpasli/veerpasli/doctype/pdf_page/pdf_page.js
+++ veerpasli/veerpasli/doctype/pdf_page/pdf_page.js
@@ -4,20 +4,22 @@
 frappe.ui.form.on("Pdf page", {
 	refresh(frm) {
 		if (frm.doc.page_file) {
-			frm.add_custom_button(__('Process Image'), function() {
-				frm.call({
-					method: "trigger_process_image",
-					doc: frm.doc,
-					callback: function(r) {
-						if (!r.exc) {
-							frappe.show_alert({
-								message: __('Image processing job scheduled'),
-								indicator: 'green'
-							});
+			if(frm.doc.status == 'pending') {
+				frm.add_custom_button(__('Process Image'), function() {
+					frm.call({
+						method: "trigger_process_image",
+						doc: frm.doc,
+						callback: function(r) {
+							if (!r.exc) {
+								frappe.show_alert({
+									message: __('Image processing job scheduled'),
+									indicator: 'green'
+								});
+							}
 						}
-					}
+					});
 				});
-			});
+			}
 
 			if (['completed', 'verified'].includes(frm.doc.status)) {
 				frm.add_custom_button(__('Open OCR Checker'), function() {
